a6024a41b6205606f1b548d100dc3b343b676d2f
yeojin_jeon <jeonyeojin1993@gmail.com>
2024-12-23 16:05:30 +0900

    [feat] 화면 구성

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import StackNavigation from './navigations/Stack';
+
+const App = () => {
+    return (
+        <NavigationContainer>
+            <StackNavigation />
+        </NavigationContainer>
+    )
+}
+
+export default App;
