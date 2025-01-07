68ef2c5956f573ae384a86d84607bec73788a61c
yeojin_jeon <jeonyeojin1993@gmail.com>
2025-01-08 07:35:10 +0900

    tab Icon

--- src/navigations/Tab.js
+++ src/navigations/Tab.js
@@ -1,14 +1,24 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
 import { Mail, Meet, Settings } from "../screens/TabScreens";
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
+const TabIcon = ({ name, size, color }) => {
+    return <MaterialCommunityIcons name={name} size={size} color={color} />;
+}
 
 const Tab = createBottomTabNavigator();
+
 const TabNavigation = () => {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="Mail" component={Mail} />
-            <Tab.Screen name="Meet" component={Meet} />
-            <Tab.Screen name="Settings" component={Settings} />
+        <Tab.Navigator initialRouteName="Settings">
+            <Tab.Screen name="Mail" component={Mail}
+                options={{ tabBarIcon: props => TabIcon({ ...props, name: 'email' }) }} />
+            <Tab.Screen name="Meet" component={Meet}
+                options={{ tabBarIcon: props => TabIcon({ ...props, name: 'video' }) }} />
+            <Tab.Screen name="Settings" component={Settings}
+                options={{ tabBarIcon: props => TabIcon({ ...props, name: 'settings' }) }} />
         </Tab.Navigator>
     );
 };
